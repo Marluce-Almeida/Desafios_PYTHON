b1b91f3e1d690474fd837ee3dc6777ec93e2b723
Marluce-Almeida <souzamarluce437@gmail.com>
2025-12-14 00:03:56 -0300

    Desafio_Tinta

---
+++ Desafio.Tinta.py
@@ -0,0 +1,16 @@
+
+# calcula quantidade de tinta para pintar uma parede  (altura x largura)/rendimento
+
+rendimento = int(input('Qual é o rendimento da lata? '))
+
+altura = int(input('Qual é a altura da parede? '))
+
+largura = int(input('Qual é a largura da parede? '))
+
+def calculo_tinta():
+    area = altura * largura
+    total = area/rendimento
+    print(f'Você precisa de {total} latas de tinta!')
+
+calculo_tinta()
+    
